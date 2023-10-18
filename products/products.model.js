e5b4a06bec34776983cf5b8f91b96b12a61156c6
silviu-axiobit <silviu@axiobit.com>
2023-10-18 11:42:08 +0300

    finished the implementation of graphql e-commerce project

--- products/products.model.js
+++ products/products.model.js
@@ -5,11 +5,56 @@ const products = [
     price: 42.12,
     reviews: [],
   },
-  { id: "bluejean", description: "Blue Jeans", price: 55.55 },
+  { id: "bluejean", description: "Blue Jeans", price: 55.55, reviews: [] },
 ];
 
 function getAllProducts() {
   return products;
 }
 
-module.exports = { getAllProducts };
+function getProductsByPrice(min, max) {
+  return products.filter((product) => {
+    return product.price >= min && product.price <= max;
+  });
+}
+
+function getProductById(id) {
+  return products.find((product) => {
+    return product.id === id;
+  });
+}
+
+function addNewProduct(id, description, price) {
+  const newProduct = {
+    id,
+    price,
+    description,
+    reviews: [],
+  };
+
+  products.push(newProduct);
+  return newProduct;
+}
+
+function addNewProductReview(id, rating, comment) {
+  const matchedProduct = getProductById(id);
+
+  if (matchedProduct) {
+    const newProductReview = {
+      rating,
+      comment,
+    };
+
+    matchedProduct.reviews.push(newProductReview);
+
+    return newProductReview;
+  }
+}
+
+module.exports = {
+  getAllProducts,
+  getProductsByPrice,
+  getProductById,
+  addNewProduct,
+  addNewProductReview,
+};
--- products/products.resolvers.js
+++ products/products.resolvers.js
@@ -5,5 +5,24 @@ module.exports = {
     products: () => {
       return productModel.getAllProducts();
     },
+    productsByPrice: (_, args) => {
+      return productModel.getProductsByPrice(args.min, args.max);
+    },
+    product: (_, args) => {
+      return productModel.getProductById(args.id);
+    },
+  },
+
+  Mutation: {
+    addNewProduct: (_, args) => {
+      return productModel.addNewProduct(args.id, args.description, args.price);
+    },
+    addNewProductReview: (_, args) => {
+      return productModel.addNewProductReview(
+        args.id,
+        args.rating,
+        args.comment
+      );
+    },
   },
 };
